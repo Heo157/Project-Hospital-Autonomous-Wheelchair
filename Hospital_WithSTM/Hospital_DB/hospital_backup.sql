ae515dd0bc76cb32787acf4304be2d348b30a356
songgu <yhs06001@naver.com>
2026-01-02 17:00:59 +0900

    fix_db

--- Hospital_WithSTM/Hospital_DB/hospital_backup.sql
+++ Hospital_WithSTM/Hospital_DB/hospital_backup.sql
@@ -91,7 +91,7 @@ CREATE TABLE `call_queue` (
   `is_dispatched` int(11) DEFAULT 0,
   `eta` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`call_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=90 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=296 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,6 +101,65 @@ CREATE TABLE `call_queue` (
 LOCK TABLES `call_queue` WRITE;
 /*!40000 ALTER TABLE `call_queue` DISABLE KEYS */;
 set autocommit=0;
+INSERT INTO `call_queue` VALUES
+(205,'2025-12-31 12:34:28','민성쿤','정형외과','재활의학과',1,'이동중'),
+(206,'2025-12-31 12:48:55','민성쿤','정형외과','재활의학과',1,'이동중'),
+(207,'2025-12-31 12:50:17','민성쿤','정형외과','재활의학과',1,'이동중'),
+(208,'2025-12-31 12:54:01','민성쿤','키오스크','재활의학과',1,'이동중'),
+(209,'2025-12-31 12:55:04','안창회','입원실','정형외과',1,'이동중'),
+(210,'2025-12-31 13:22:33','안창회','재활의학과','키오스크',1,'이동중'),
+(211,'2025-12-31 13:22:39','서민솔','정형외과','입원실',1,'이동중'),
+(212,'2025-12-31 13:22:46','서민솔','키오스크','식당',1,'이동중'),
+(213,'2025-12-31 13:49:16','리두현','입원실','정형외과',1,'이동중'),
+(214,'2025-12-31 13:51:30','리두현','입원실','정형외과',1,'이동중'),
+(215,'2025-12-31 13:55:17','나지훈','식당','정형외과',1,'이동중'),
+(216,'2025-12-31 13:59:23','나지훈','재활의학과','입원실',1,'이동중'),
+(217,'2025-12-31 14:09:34','나지훈','재활의학과','입원실',1,'이동중'),
+(218,'2025-12-31 14:14:33','나지훈','재활의학과','입원실',1,'이동중'),
+(219,'2025-12-31 14:33:58','나지훈','재활의학과','입원실',1,'이동중'),
+(220,'2025-12-31 14:40:57','리두현','정형외과','입원실',1,'이동중'),
+(221,'2025-12-31 14:43:39','민성쿤','키오스크','입원실',1,'이동중'),
+(222,'2025-12-31 14:48:14','민성쿤','키오스크','입원실',1,'이동중'),
+(223,'2025-12-31 14:48:54','민성쿤','키오스크','입원실',1,'이동중'),
+(224,'2025-12-31 14:54:10','민성쿤','키오스크','입원실',1,'이동중'),
+(225,'2025-12-31 14:55:40','서민솔','정형외과','키오스크',1,'이동중'),
+(226,'2025-12-31 15:03:41','외래환자','키오스크','정형외과',1,'이동중'),
+(227,'2025-12-31 15:04:42','외래환자','키오스크','식당',1,'이동중'),
+(228,'2025-12-31 15:07:05','외래환자','키오스크','입원실',1,'이동중'),
+(229,'2025-12-31 15:15:00','강송구','키오스크','재활의학과',1,'이동중'),
+(230,'2025-12-31 15:28:03','서민솔','식당','재활의학과',1,'이동중'),
+(231,'2025-12-31 15:32:43','서민솔','식당','재활의학과',1,'이동중'),
+(232,'2025-12-31 15:47:08','외래환자','키오스크','재활의학과',1,'이동중'),
+(233,'2025-12-31 16:04:30','서민솔','식당','재활의학과',1,'이동중'),
+(234,'2025-12-31 16:14:30','서민솔','재활의학과','식당',1,'이동중'),
+(235,'2025-12-31 16:15:22','안창회','재활의학과','입원실',1,'이동중'),
+(236,'2025-12-31 16:18:15','안창회','정형외과','키오스크',1,'이동중'),
+(237,'2025-12-31 16:18:23','외래환자','키오스크','재활의학과',1,'이동중'),
+(238,'2025-12-31 18:02:32','안창회','정형외과','키오스크',1,'이동중'),
+(239,'2025-12-31 18:03:49','외래환자','키오스크','재활의학과',1,'이동중'),
+(240,'2025-12-31 18:04:05','외래환자','키오스크','식당',1,'이동중'),
+(241,'2025-12-31 18:04:22','리두현','식당','재활의학과',1,'이동중'),
+(243,'2025-12-31 18:07:47','외래환자','키오스크','정형외과',1,'이동중'),
+(245,'2025-12-31 18:15:05','리두현','정형외과','재활의학과',1,'이동중'),
+(276,'2025-12-31 20:47:07','강송구','재활의학과','키오스크',1,'이동중'),
+(277,'2025-12-31 20:51:07','강송구','식당','키오스크',1,'이동중'),
+(278,'2025-12-31 20:51:20','문두르','재활의학과','입원실',1,'이동중'),
+(279,'2025-12-31 20:59:15','외래환자','키오스크','입원실',1,'이동중'),
+(280,'2025-12-31 21:00:18','문두르','식당','정형외과',1,'이동중'),
+(281,'2025-12-31 21:01:43','문두르','재활의학과','키오스크',1,'이동중'),
+(282,'2025-12-31 21:02:53','문두르','입원실','정형외과',1,'이동중'),
+(283,'2025-12-31 21:03:05','리두현','재활의학과','입원실',1,'이동중'),
+(285,'2026-01-02 12:20:59','강송구','키오스크','재활의학과',1,'이동중'),
+(286,'2026-01-02 12:27:57','강송구','정형외과','재활의학과',1,'이동중'),
+(287,'2026-01-02 12:29:11','강송구','키오스크','입원실',1,'이동중'),
+(288,'2026-01-02 12:30:29','외래환자','키오스크','입원실',1,'이동중'),
+(289,'2026-01-02 12:32:18','외래환자','키오스크','재활의학과',1,'이동중'),
+(290,'2026-01-02 13:01:19','외래환자','키오스크','재활의학과',1,'이동중'),
+(291,'2026-01-02 13:14:01','서채건','키오스크','입원실',1,'이동중'),
+(292,'2026-01-02 13:15:05','강송구','키오스크','입원실',1,'이동중'),
+(293,'2026-01-02 13:16:30','외래환자','키오스크','입원실',1,'이동중'),
+(294,'2026-01-02 13:17:07','강송구','재활의학과','입원실',1,'이동중'),
+(295,'2026-01-02 13:18:55','외래환자','키오스크','식당',1,'이동중');
 /*!40000 ALTER TABLE `call_queue` ENABLE KEYS */;
 UNLOCK TABLES;
 commit;
@@ -173,39 +232,19 @@ UNLOCK TABLES;
 commit;
 
 --
--- Table structure for table `map_location`
+-- Temporary table structure for view `map_location`
 --
 
 DROP TABLE IF EXISTS `map_location`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8mb4 */;
-CREATE TABLE `map_location` (
-  `location_id` int(11) NOT NULL AUTO_INCREMENT,
-  `location_name` varchar(50) DEFAULT NULL,
-  `x` double DEFAULT NULL,
-  `y` double DEFAULT NULL,
-  PRIMARY KEY (`location_id`),
-  UNIQUE KEY `location_name` (`location_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `map_location`
---
-
-LOCK TABLES `map_location` WRITE;
-/*!40000 ALTER TABLE `map_location` DISABLE KEYS */;
-set autocommit=0;
-INSERT INTO `map_location` VALUES
-(1,'정형외과',0.372,-2.86),
-(2,'식당',1.38,-0.65),
-(3,'키오스크',0,0),
-(4,'재활의학과',0.4,-3.6),
-(5,'입원실',1.57,-2.8),
-(6,'충전스테이션',0,0);
-/*!40000 ALTER TABLE `map_location` ENABLE KEYS */;
-UNLOCK TABLES;
-commit;
+/*!50001 DROP VIEW IF EXISTS `map_location`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8mb4;
+/*!50001 CREATE VIEW `map_location` AS SELECT
+ 1 AS `location_id`,
+  1 AS `location_name`,
+  1 AS `x`,
+  1 AS `y` */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `patient_info`
@@ -241,7 +280,7 @@ INSERT INTO `patient_info` VALUES
 ('P1002','리두현','RM001',0,'IN','5동',1,'2025-12-22 11:04:51'),
 ('P1003','홍진경','OS001',0,'IN','5동',2,'2025-12-18 21:35:34'),
 ('P1004','민성쿤','RM003',0,'OUT',NULL,NULL,NULL),
-('P1005','나찌','RM001',1,'IN','5동',3,'2025-12-19 21:32:20'),
+('P1005','나지훈','RM001',1,'IN','5동',3,'2025-12-19 00:00:00'),
 ('P1006','문두르','OS001',0,'OUT',NULL,NULL,NULL),
 ('P1007','강송구','OS004',0,'OUT',NULL,NULL,NULL),
 ('P1008','유종민','OS002',0,'IN','5동',4,'2025-12-19 21:34:58'),
@@ -311,9 +350,11 @@ CREATE TABLE `robot_status` (
   `sensor` int(11) DEFAULT NULL,
   `order` tinyint(3) unsigned DEFAULT NULL,
   `who_called` char(20) DEFAULT NULL,
+  `ultra_distance_cm` int(11) DEFAULT 0,
+  `seat_detected` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`robot_id`),
   UNIQUE KEY `uk_robot_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=448491 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=951709 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -324,8 +365,88 @@ LOCK TABLES `robot_status` WRITE;
 /*!40000 ALTER TABLE `robot_status` DISABLE KEYS */;
 set autocommit=0;
 INSERT INTO `robot_status` VALUES
-(442826,'ubuntu','10.10.14.148','WAITING',8443,0,0.07394149899482727,-0.07017111033201218,-0.0229378379881382,0.372,-2.86,0,0,NULL,0,'admin');
+(599401,'turtlebot3','10.10.14.148','WAITING',36,0,1.872197151184082,1.8040390014648438,-2.0222225189208984,0,-0.782,2.2,1.62,NULL,0,'admin',0,0);
 /*!40000 ALTER TABLE `robot_status` ENABLE KEYS */;
+UNLOCK TABLES;
+commit;
+
+--
+-- Table structure for table `tb_edges`
+--
+
+DROP TABLE IF EXISTS `tb_edges`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_edges` (
+  `edge_id` int(11) NOT NULL AUTO_INCREMENT,
+  `node1_id` int(11) NOT NULL,
+  `node2_id` int(11) NOT NULL,
+  PRIMARY KEY (`edge_id`),
+  KEY `node1_id` (`node1_id`),
+  KEY `node2_id` (`node2_id`),
+  CONSTRAINT `tb_edges_ibfk_1` FOREIGN KEY (`node1_id`) REFERENCES `tb_waypoints` (`node_id`) ON DELETE CASCADE,
+  CONSTRAINT `tb_edges_ibfk_2` FOREIGN KEY (`node2_id`) REFERENCES `tb_waypoints` (`node_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_edges`
+--
+
+LOCK TABLES `tb_edges` WRITE;
+/*!40000 ALTER TABLE `tb_edges` DISABLE KEYS */;
+set autocommit=0;
+INSERT INTO `tb_edges` VALUES
+(1,104,3),
+(2,104,6),
+(3,104,103),
+(4,104,101),
+(5,102,103),
+(6,101,2),
+(7,101,5),
+(8,102,1),
+(9,103,4),
+(10,3,6);
+/*!40000 ALTER TABLE `tb_edges` ENABLE KEYS */;
+UNLOCK TABLES;
+commit;
+
+--
+-- Table structure for table `tb_waypoints`
+--
+
+DROP TABLE IF EXISTS `tb_waypoints`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_waypoints` (
+  `node_id` int(11) NOT NULL,
+  `node_name` varchar(50) DEFAULT NULL,
+  `x` double NOT NULL,
+  `y` double NOT NULL,
+  `is_destination` tinyint(4) DEFAULT 0,
+  PRIMARY KEY (`node_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_waypoints`
+--
+
+LOCK TABLES `tb_waypoints` WRITE;
+/*!40000 ALTER TABLE `tb_waypoints` DISABLE KEYS */;
+set autocommit=0;
+INSERT INTO `tb_waypoints` VALUES
+(1,'정형외과',2.99,-0.391,1),
+(2,'식당',2.2,1.62,1),
+(3,'키오스크',0,-0.782,1),
+(4,'재활의학과',1.85,-0.34,1),
+(5,'입원실',0.193,1.68,1),
+(6,'충전스테이션',0,0,1),
+(101,'식당_병실_앞',1.35,1.81,0),
+(102,'정형외과_앞',2.14,0.61,0),
+(103,'재활_앞',2.12,0.61,0),
+(104,'중간_포인트',1.35,0.61,0);
+/*!40000 ALTER TABLE `tb_waypoints` ENABLE KEYS */;
 UNLOCK TABLES;
 commit;
 
@@ -359,6 +480,28 @@ INSERT INTO `users` VALUES
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 commit;
+
+--
+-- Dumping routines for database 'hospital_db'
+--
+
+--
+-- Final view structure for view `map_location`
+--
+
+/*!50001 DROP VIEW IF EXISTS `map_location`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_uca1400_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`%` SQL SECURITY DEFINER */
+/*!50001 VIEW `map_location` AS select `tb_waypoints`.`node_id` AS `location_id`,`tb_waypoints`.`node_name` AS `location_name`,`tb_waypoints`.`x` AS `x`,`tb_waypoints`.`y` AS `y` from `tb_waypoints` where `tb_waypoints`.`is_destination` = 1 */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -369,4 +512,4 @@ commit;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2025-12-28 17:08:25
+-- Dump completed on 2026-01-02 16:55:59
